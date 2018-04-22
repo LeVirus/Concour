cd5a81624dc52c214818fbb8ff37f30e4f34a7be
LeVirus <1203896@gmail.com>
2018-04-22 11:26:00 +0200

    Add license.

--- form.cpp
+++ form.cpp
@@ -289,3 +289,20 @@ Form::~Form()
     if(m_memListMan)delete m_memListMan;
     if(m_memListWoman)delete m_memListWoman;
 }
+
+/**
+ * Petanque Competition Management Program
+ * Copyright (C) 2018 Cyril Charbonneau
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * his program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
--- pdfdocgeneration.cpp
+++ pdfdocgeneration.cpp
@@ -132,3 +132,20 @@ void PdfDocGeneration::generateDoc()
     doc.setPageSize(printer.pageRect().size()); // This is necessary if you want to hide the page number
     doc.print(&printer);
 }
+
+/**
+ * Petanque Competition Management Program
+ * Copyright (C) 2018 Cyril Charbonneau
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * his program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
